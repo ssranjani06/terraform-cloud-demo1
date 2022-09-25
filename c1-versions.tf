84bcf5ccdde4c7137c71b92b92be98064dc045a6
ssranjani06 <99155402+ssranjani06@users.noreply.github.com>
2022-09-25 14:51:00 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -11,8 +11,8 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
-  profile = "default"
+  region  = "us-east-1"
+  #profile = "default"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
